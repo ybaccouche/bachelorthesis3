10c473c5d7496cee8a591f0b74f61c1b3679898a
ybaccouche <youssefbaccou@gmail.com>
2024-06-18 13:18:16 +0200

    new

--- jobs/scripts/simple1.sh
+++ jobs/scripts/simple1.sh
@@ -4,7 +4,6 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=defq
-#SBATCH -C <resource name, see below> # this line is usually not needed
 #SBATCH --gres=gpu:1
 
 # Load GPU drivers
